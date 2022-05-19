b9f38c8bc996d1d014dd742e741540f465199cb2
leonorcduarte <lc.duarte@campus.fct.unl.pt>
2022-05-19 12:30:19 +0100

    Add base classes

---
+++ app/src/main/java/com/example/mypokedex/ui/pokedetail/PokeDetailFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mypokedex.ui.pokedetail
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PokeDetailFragment: Fragment() {
+}
