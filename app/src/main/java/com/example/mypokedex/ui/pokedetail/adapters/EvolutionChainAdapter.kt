9424f3b4df990781fda4671a4da27ae1e8f9a724
leonorcduarte <lc.duarte@campus.fct.unl.pt>
2022-05-20 17:41:41 +0100

    Add colors to evolutionChain; change detailScreen organization

--- app/src/main/java/com/example/mypokedex/ui/pokedetail/adapters/EvolutionChainAdapter.kt
+++ app/src/main/java/com/example/mypokedex/ui/pokedetail/adapters/EvolutionChainAdapter.kt
@@ -5,11 +5,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mypokedex.R
 
 class EvolutionChainAdapter(
-    private var evolutionsList: List<String>
+    private var evolutionsList: List<String>,
+    private var colorPair: Pair<String, List<Int?>>
 ): RecyclerView.Adapter<EvolutionChainAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -18,11 +20,15 @@ class EvolutionChainAdapter(
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         private val evolutionName: TextView = itemView.findViewById(R.id.pokemon_name)
+        private val nameContainer: ConstraintLayout = itemView.findViewById(R.id.name_container)
         private val arrow: ImageView = itemView.findViewById(R.id.next_arrow)
 
         fun bind(position: Int, name: String){
             evolutionName.text = name
             arrow.visibility = if (position == itemCount - 1) View.GONE else View.VISIBLE
+
+            colorPair.second[0]?.let { nameContainer.background.setTint(it) }
+            colorPair.second[1]?.let { arrow.setColorFilter(it) }
         }
     }
 
